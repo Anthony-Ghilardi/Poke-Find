4d6281c2ffe13074387c65a056d799deb77cdce0
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-10-09 13:16:47 -0700

    Completed more routes for pokemon information

--- backend/routes/requests.js
+++ backend/routes/requests.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get("/", async (req,res) => { 
     try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/arceus")
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/charmander")
         const data = await response.json();
         res.json(data);
     } catch (error) {
@@ -11,5 +11,43 @@ router.get("/", async (req,res) => {
         res.status(500).json({ error: "Failed to fetch pokemon data "});
     }
 });
+
+router.get("/types", async (req, res) => {
+    try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/charmander")
+        const data = await response.json();
+        const types = data.types;
+        res.json(types);
+    } catch (error) {
+        console.error("Error fetching data from Pokemon API", error);
+        res.status(500).json({ error: "Failed to fetch pokemon data"})
+    }
+});
+
+router.get("/sprites", async (req, res) => {
+    try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon-form/charmander")
+        const data = await response.json();
+        const sprites = data.sprites;
+        res.json(sprites);
+    } catch (error) {
+        console.error("Error fetching data from Pokemon API", error);
+        res.status(500).json({ error: "Failed to fetch pokemon data"})
+    }
+});
+
+router.get("/description", async (req, res) => {
+    try {
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon-species/charmander")
+        const data = await response.json();
+        
+        const englishFlavorText = data.flavor_text_entries.find(entry => entry.language.name === "en");
+        const description = englishFlavorText ? englishFlavorText.flavor_text : "No description available";
+        res.json({ description });
+    } catch (error) {
+        console.error("Error fetching data from Pokemon API", error);
+        res.status(500).json({ error: "Failed to fetch pokemon data"})
+    }
+})
 
 module.exports = router;
--- frontend/src/components/RouteTests/RouteTests.jsx
+++ frontend/src/components/RouteTests/RouteTests.jsx
@@ -13,7 +13,7 @@ export default function RouteTests() {
       setPokemonName(data);
       console.log(data);
     } catch (error) {
-      console.error("Error fetching data", error);
+      console.error("Error fetching name", error);
     }
   }
 
@@ -21,9 +21,84 @@ export default function RouteTests() {
     fetchName();
   }, []);
 
+  const [pokemonType, setPokemonType] = useState(null);
+
+  async function fetchType() {
+    try {
+      const response = await fetch("/pokemon/types");
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+      const data = await response.json();
+      setPokemonType(data);
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching types", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchType();
+  }, []);
+
+  const [pokemonSprite, setPokemonSprite] = useState(null);
+
+  async function fetchSprite() {
+    try {
+      const response = await fetch("/pokemon/sprites");
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+      const data = await response.json();
+      setPokemonSprite(data);
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching sprite", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchSprite();
+  }, []);
+
+  const [pokemonDescription, setPokemonDescription] = useState(null);
+
+  async function fetchDescription() {
+    try {
+      const response = await fetch ("/pokemon/description");
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+      const data = await response.json();
+      setPokemonDescription(data);
+      console.log(data)
+    } catch (error) {
+      console.error("Error description sprite", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchDescription();
+  }, []);
+
   return (
     <div>
-      <h1>{pokemonName ? pokemonName.name : "Loading..."}</h1>
+      <h1>Pokemon Name: {pokemonName ? pokemonName.name : "Loading..."}</h1>
+      <h2>
+        Pokemon Type:
+        {pokemonType
+          ? pokemonType.map((i, index) => (
+              <span key={index}> {i.type.name} </span>
+            ))
+          : "Loading..."}
+      </h2>
+      <h3>Pokemon Sprite:</h3>
+      {pokemonSprite ? (
+        <img src={pokemonSprite.front_default} alt="Pokemon Sprite" />
+      ) : (
+        "Loading..."
+      )}
+      <h4>Pokemon Description: {pokemonDescription ? pokemonDescription.description : "Loading..."}</h4>
     </div>
   );
 }
