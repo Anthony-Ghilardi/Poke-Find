81f2533af8c572a87a99bc9f978b6ff9da7cbfda
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-11-02 20:06:41 -0700

    Docker deployment configs

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const requests = require('./routes/requests');
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+// Enable CORS for all routes
+app.use(cors({
+    origin: 'https://poke-find-production.up.railway.app'
+}));
 
 app.use(express.json());
 app.use('/pokemon', requests);
