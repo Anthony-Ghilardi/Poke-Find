3db57648c9fe437b5dafd6e809b9b7a2a958e872
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-12-03 11:01:50 -0800

    First db connection added

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const { Pool } = require('pg');
 const cors = require('cors');
 require('dotenv').config();
 const requests = require('./routes/requests');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// Deployment devlopment cor setup
+// Deployment devlopment cors setup
 // app.use(cors({
 //     origin: 'https://poke-find-production.up.railway.app'
 // }));
@@ -15,6 +16,32 @@ app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.use(express.json());
 app.use('/pokemon', requests);
+
+const db = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+db.connect((err, client, done) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.stack);
+    } else {
+        console.log('Connected to PostgreSQL database');
+    }
+    done();
+});
+
+app.get('/', (req, res) => {
+    return res.json("Hi from pqsql backend");
+})
+
+app.get('/users', async (req, res) => {
+    const result = await db.query ("SELECT * FROM users");
+    res.json(result.rows);
+})
 
 app.listen(PORT, () => {
     console.log(`The server is running on port ${PORT}`);
